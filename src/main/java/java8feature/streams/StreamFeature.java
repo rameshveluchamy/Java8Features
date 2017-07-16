a5c8d862ebed38422f7ccedd8b36f855b94683af
rameshveluchamy <rameshkumar.v19@gmail.com>
2017-07-16 12:00:33 +0100

    commit

--- src/main/java/java8feature/streams/StreamFeature.java
+++ src/main/java/java8feature/streams/StreamFeature.java
@@ -1,6 +1,7 @@
 package main.java.java8feature.streams;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by ramesh on 15/07/17.
@@ -8,12 +9,39 @@ import java.util.stream.Stream;
 public class StreamFeature {
 
     public static void main(String[] args) {
+        List<Person> contestants = Arrays.asList(
+                new Person("Mark", "Antony", 23, 'M', "Italian"),
+                new Person("Mahi", "Dhoni", 35, 'M', "Indian"),
+                new Person("Gayu", "Ram", 28, 'F', "Indian"),
+                new Person("Aaru", "Kumar", 25, 'M', "Indian"),
+                new Person("Julie", "Kruger", 23, 'F', "Indian"),
+                new Person("Kate", "Winslet", 40, 'F', "British"));
 
-        Stream.of(
-                new Person("Mark", "Antony", 40, 'M', "Italian")
-        ).count();
+        /*for (Person p : contestants) {
+            System.out.println(p.toString());
+        }*/
+
+        System.out.println("---------------List all contestants------------------");
+
+        contestants.stream()
+                .forEach(person -> System.out.println(person.toString()));
+
+        System.out.println("---------------List all Male contestants------------------");
+        contestants.stream()
+                .filter(person -> person.getSex() == 'M')
+                .forEach(person -> System.out.println(person.toString()));
+
+        System.out.println("---------------Youngest Female contestants------------------");
+
+        contestants.stream()
+                .filter(p -> p.getSex() == 'F')
+                .sorted((person, t1) -> person.getAge())
+                .forEach(person -> System.out.println(person.toString()));//TODO: to be corrected
+
+        System.out.println("---------------Eldest Male contestants------------------");
+        System.out.println("---------------Average Age of Female contestants------------------");
+        System.out.println("---------------List contestants sorted by nationality------------------");
     }
-
 }
 
 class Person {
@@ -36,39 +64,30 @@ class Person {
         return fname;
     }
 
-    public void setFname(String fname) {
-        this.fname = fname;
-    }
-
     public String getLname() {
         return lname;
-    }
-
-    public void setLname(String lname) {
-        this.lname = lname;
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public char getSex() {
         return sex;
-    }
-
-    public void setSex(char sex) {
-        this.sex = sex;
     }
 
     public String getNationality() {
         return nationality;
     }
 
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
+    @Override
+    public String toString() {
+        return "Person{" +
+                "fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                ", nationality='" + nationality + '\'' +
+                '}';
     }
 }
